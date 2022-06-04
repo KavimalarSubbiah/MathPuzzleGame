0cafaffc1af9e20015565ed02c15b851d3b40d8f
KavimalarSubbiah <96462635+KavimalarSubbiah@users.noreply.github.com>
2022-06-04 23:59:06 +0530

    MathPuzzleApp

--- app/src/main/java/com/example/deltatask1/MainActivity.java
+++ app/src/main/java/com/example/deltatask1/MainActivity.java
@@ -414,7 +414,14 @@ public class MainActivity<array, intArray> extends AppCompatActivity {
         alertDialog.show();
     }
     public void OnFalse2(){
-
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+// Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
 
         AlertDialog.Builder builder
                 = new AlertDialog
